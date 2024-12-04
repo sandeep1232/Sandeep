774cd4df999133fd20fcc61a8a60e30fb7665847
sandeep <sandeep.mudulagar@gmail.com>
2024-12-04 17:46:46 +0530

    Add project files.

---
+++ Sandeep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sandeep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
